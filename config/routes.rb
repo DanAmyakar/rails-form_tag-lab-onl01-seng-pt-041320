4b9f491ca5358f914951aebf4475467071e35628
DanAmyakar <dan.rockit@gmail.com>
2020-08-28 15:58:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+  resources :students, only: [:index, :create, :new]
+  # routes from 'resources :studetns'
+  #// ♥  rake routes
+  #    Prefix   Verb   URI Pattern                  Controller#Action
+  #  students   GET    /students(.:format)          students#index
+  #             POST   /students(.:format)          students#create
+  #new_student  GET    /students/new(.:format)      students#new
+  #edit_student GET    /students/:id/edit(.:format) students#edit
+  #   student   GET    /students/:id(.:format)      students#show
+  #             PATCH  /students/:id(.:format)      students#update
+  #             PUT    /students/:id(.:format)      students#update
+  #             DELETE /students/:id(.:format)      students#destroy
+
   get '/students/:id', to: 'students#show', as: 'student'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student1 = Student.new(first_name: "stud_one", last_name: "ent_one").save
+student2 = Student.new(first_name: "stud_two", last_name: "ent_two").save
+student3 = Student.new(first_name: "stud_tre", last_name: "ent_tre").save
+student4 = Student.new(first_name: "stud_for", last_name: "ent_for").save
+student5 = Student.new(first_name: "stud_fie", last_name: "ent_fie").save
